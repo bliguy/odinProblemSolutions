d786d2a35b2c3dfc908d168b6dd2f874334d4a9b
Mike Bleier <mbleier@gmail.com>
2020-07-28 15:55:37 -0500

    test

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+function sumAll(a, b) {
+    let output = a + b
+    let difference = b - a
+    let minusOne = b - 1
+    if (a = b) {
+        output = a + b
+    }
+    else if (a < b) {
+        for (let i = difference; difference > 0; difference -- )
+            let subTotal += output + minusOne
+            minusOne --
+            output += subTotal
+        } 
+    } else {
+        let difference2 = a - b
+        let minusOne2 = a - 1
+        do {
+            output += minusOne2
+            difference2--
+        } while (difference2 > 0 && minusOne2 > b) 
+    }
+    return output
 }
-
 module.exports = sumAll
