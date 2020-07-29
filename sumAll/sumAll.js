627a6657c526a9d24b291b62a19ce9355b57801d
Mike Bleier <mbleier@gmail.com>
2020-07-29 10:45:04 -0500

    logic error

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -2,23 +2,24 @@ function sumAll(a, b) {
     let output = a + b
     let difference = b - a
     let minusOne = b - 1
+    let subTotal
     if (a = b) {
         output = a + b
     }
     else if (a < b) {
         for (let i = difference; difference > 0; difference -- )
-            let subTotal += output + minusOne
+            subTotal += output + minusOne
             minusOne --
             output += subTotal
         } 
-    } else {
-        let difference2 = a - b
-        let minusOne2 = a - 1
-        do {
-            output += minusOne2
-            difference2--
-        } while (difference2 > 0 && minusOne2 > b) 
-    }
+    // } else {
+    //     let difference2 = a - b
+    //     let minusOne2 = a - 1
+    //     do {
+    //         output += minusOne2
+    //         difference2--
+    //     } while (difference2 > 0 && minusOne2 > b) 
+    // }
     return output
 }
 module.exports = sumAll
